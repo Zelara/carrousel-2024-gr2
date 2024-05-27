f37d92e5c55b8d4beaf3298e226d200f1ea10025
Zelara <119063805+Zelara@users.noreply.github.com>
2024-05-27 11:44:10 -0400

    Revenu a la vieille version il avait des bogues

--- js/carrousel.js
+++ js/carrousel.js
@@ -6,31 +6,35 @@
     let carrousel__figure = document.querySelector(".carrousel__figure");
     let galerie__img = galerie.querySelectorAll("img");
 
+    let index = 0;
     for (const elm of galerie__img) {
-      creer_image_carrousel(elm);
-      creer_radio_carrousel(elm);
+      creer_image_carrousel(index, elm);
+      creer_radio_carrousel(index);
       elm.addEventListener("click", function () {
-        ouvrir_carrousel(elm);
+        ouvrir_carrousel(index);
       });
+      index = index + 1;
     }
 
-    function creer_image_carrousel(elm) {
+    function creer_image_carrousel(index, elm) {
       let carrousel__img = document.createElement("img");
       carrousel__img.classList.add("carrousel__img");
+      carrousel__img.dataset.index = index;
       carrousel__img.src = elm.src;
       carrousel__figure.appendChild(carrousel__img);
     }
 
-    function creer_radio_carrousel(elm) {
+    function creer_radio_carrousel(index) {
       let carrousel__radio = document.createElement("input");
       carrousel__radio.type = "radio";
       carrousel__radio.name = "carrousel_radio";
+      carrousel__radio.dataset.index = index;
 
       carrousel__radio.addEventListener("change", function () {
         let images = document.querySelectorAll(".carrousel__img");
         images.forEach((img) => (img.style.opacity = 0));
         let selectedImg = document.querySelector(
-          `.carrousel__img[src="${elm.src}"]`
+          `.carrousel__img[data-index="${index}"]`
         );
         if (selectedImg) selectedImg.style.opacity = 1;
       });
@@ -39,14 +43,15 @@
       carrousel__form.appendChild(carrousel__radio);
     }
 
-    function ouvrir_carrousel(elm) {
+    function ouvrir_carrousel(index) {
       carrousel.classList.add("carrousel--ouvrir");
-      let selectedImg = document.querySelector(
-        `.carrousel__img[src="${elm.src}"]`
-      );
-      if (selectedImg) {
-        selectedImg.style.opacity = 1;
-      }
+      document.querySelector(
+        `input[name='carrousel_radio'][data-index='${index}']`
+      ).checked = true;
+      let event = new Event("change");
+      document
+        .querySelector(`input[name='carrousel_radio'][data-index='${index}']`)
+        .dispatchEvent(event);
     }
 
     // Fermer le carrousel en cliquant en dehors des composants internes spécifiques
